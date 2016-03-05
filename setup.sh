fa23431c30557ccad49868dca988b0008a9a52fb
Kurt Hsu <kurt.hsu@prophetstor.com>
2016-03-06 07:42:36 +0800

    add oh-my-zsh and vim as submodules, and update installation flow

--- setup.sh
+++ setup.sh
@@ -1,43 +1,26 @@
 #!/bin/bash
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+# ==========================================================================================
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# ==========================================================================================
+
+# ================= Variables ====================
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim tmux.conf zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
-##########
-
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
-
+# ================= Functions ====================
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    # Clone oh-my-zsh repository from GitHub only if it isn't already present
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
         git clone http://github.com/robbyrussell/oh-my-zsh.git
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        #chsh -s $(which zsh)
+        chsh -s $(which zsh)
         echo $(which zsh)
     fi
 else
@@ -55,7 +38,35 @@ else
 fi
 }
 
-# install_zsh
+# ================= Main Flow ====================
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# install oh-my-zsh and 
+install_zsh
+
+# Clone my vimrc repository from GitHub only if it isn't already present
+if [[ ! -d $dir/vim/ ]]; then
+    git clone https://github.com/kurthsu/vimrc.git $dir/vim
+fi
+cp $dir/vim/vimrc $dir/vimrc
+
+# move any existing dotfiles in homedir to dotfiles_old directory, 
+# then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
 
 # Install lib for ctags
 apt-get install exuberant-ctags
